12d880ee82b9d447ea7cd41812f8d589b2368465
Spencer <spencerb242@proton.me>
2023-01-04 12:34:49 -0700

    created custom hook for fetching data and implemented it into TripList component

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,34 +1,24 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
 
 //styles
 import "./TripList.css";
 
 export default function TripList() {
-  const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState("http://localhost:3000/trips");
-
-  const fetchTrips = useCallback(async () => {
-    const response = await fetch(url);
-    const json = await response.json();
-    setTrips(json);
-  }, [url]);
-
-  useEffect(() => {
-    fetchTrips();
-  }, [url]);
-
-  console.log(trips);
+  const { data: trips } = useFetch(url);
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
       <ul>
-        {trips.map((trip) => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
       </ul>
       <div className="filters">
         <button
